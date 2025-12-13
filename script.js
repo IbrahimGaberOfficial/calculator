55ff4e4a64428f0f201c5593d90bb3e7c9326b19
IbrahimGaberOfficial <ibrahim.gaber.official@gmail.com>
2025-12-13 08:03:35 +0200

    adding the basic functions for [add, subtract, multiply, divide].

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello console")
+function add(num1=0, num2=0){
+    return num1 + num2;
+}
+
+function subtract(num1=0, num2=0){
+    return num1 - num2;
+}
+
+function multiply(num1=1, num2=1){
+    return num1 * num2;
+}
+
+function divide(num1=1, num2=1){
+    return num1 / num2;
+}
